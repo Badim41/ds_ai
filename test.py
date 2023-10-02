6dca2c7e73d14b78ec7e34810b3ff8b0042fdb60
badim41 <ramdomakk@gmail.com>
2023-10-03 10:25:13 +1200

    added-2

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-from gpt4all import GPT4All
-model = GPT4All("orca-mini-3b.ggmlv3.q4_0.bin")
-output = model.generate("The capital of France is ", max_tokens=3)
-print(output)
+import torch
+
+def get_available_gpus():
+    if torch.cuda.is_available():
+        gpu_count = torch.cuda.device_count()
+        available_gpus = [torch.cuda.get_device_name(i) for i in range(gpu_count)]
+        return available_gpus
+    else:
+        return []
+
+available_gpus = get_available_gpus()
+if available_gpus:
+    print("Доступные GPU:")
+    for gpu in available_gpus:
+        print(gpu)
+else:
+    print("GPU не найдены.")
