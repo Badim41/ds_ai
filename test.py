8910f9e7193110ab48a1f5a42d6920471a1c3586
badim41 <ramdomakk@gmail.com>
2023-10-03 09:28:13 +1200

    added-2

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-from pathlib import Path
 from gpt4all import GPT4All
-
-model_name = 'orca-mini-3b.ggmlv3.q4_0.bin'
-model_path = Path.home() / '.local' / 'share' / 'nomic.ai' / 'GPT4All'
-model = GPT4All(model_name, model_path)
+model = GPT4All("orca-mini-3b.ggmlv3.q4_0.bin")
+output = model.generate("The capital of France is ", max_tokens=3)
+print(output)
