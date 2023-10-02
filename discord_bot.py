db96391b62179c6f5603bce8be337f2d1e010443
badim41 <ramdomakk@gmail.com>
2023-10-03 08:39:38 +1200

    added-2

--- discord_bot.py
+++ discord_bot.py
@@ -62,18 +62,17 @@ async def say(ctx, *args):
     else:
         print(message)
     message = await replace_mat_in_sentence(message)
-    print(type(message))
     # Проверяем, находится ли автор команды в войс-чате
     if ctx.author.voice:
         # Получаем войс-канал автора команды
         voice_channel = ctx.author.voice.channel
         # Проверяем, находится ли бот уже в каком-либо войс-чате
-        if ctx.voice_client is None:
-            # Если бот не находится в войс-чате, подключаем его
-            await voice_channel.connect()
-        else:
-            # Если бот уже находится в войс-чате, перемещаем его в новый войс-канал
-            await ctx.voice_client.move_to(voice_channel)
+        # if ctx.voice_client is None:
+        #     # Если бот не находится в войс-чате, подключаем его
+        #     await voice_channel.connect()
+        # else:
+        #     # Если бот уже находится в войс-чате, перемещаем его в новый войс-канал
+        #     await ctx.voice_client.move_to(voice_channel)
         await run_main_with_settings(ctx, message, True)
     else:
         await ctx.send("Вы должны находиться в войс-чате, чтобы использовать эту команду.")
