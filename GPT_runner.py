c05db9b378377c3bb7dbb3863e028bc053af5916
badim41 <ramdomakk@gmail.com>
2023-10-03 21:32:22 +1200

    updated

--- GPT_runner.py
+++ GPT_runner.py
@@ -14,7 +14,6 @@ def run():
                                                use_safetensors=True,
                                                trust_remote_code=True,
                                                device="cuda:0",
-                                               device_map="auto",
                                                use_triton=True,
                                                quantize_config=None)
     model_loaded = True
