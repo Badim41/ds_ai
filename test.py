75da26cc0605b3c8c0e8436ab925de24c71fa586
badim41 <ramdomakk@gmail.com>
2023-10-03 16:52:50 +1200

    updated

--- test.py
+++ test.py
@@ -1,17 +1,29 @@
-import torch
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
 
-def get_available_gpus():
-    if torch.cuda.is_available():
-        gpu_count = torch.cuda.device_count()
-        available_gpus = [torch.cuda.get_device_name(i) for i in range(gpu_count)]
-        return available_gpus
-    else:
-        return []
 
-available_gpus = get_available_gpus()
-if available_gpus:
-    print("Доступные GPU:")
-    for gpu in available_gpus:
-        print(gpu)
-else:
-    print("GPU не найдены.")
+model_name = 'fffrrt/ruGPT-3.5-13B-GPTQ'
+model_basename = 'gptq_model-4bit-128g'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+model = AutoGPTQForCausalLM.from_quantized(model_name,
+        model_basename = model_basename,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device="cuda:0",
+        use_triton=False,
+        quantize_config=None)
+
+text = "Чтобы подружиться с осьминогом, нужно: \n1."
+
+encoded_input = tokenizer(text, return_tensors='pt').to('cuda:0')
+output = model.generate(
+    **encoded_input,
+    num_beams=4,
+    max_new_tokens=160,
+    no_repeat_ngram_size=2,
+    # num_return_sequences=5,
+    # do_sample=True
+)
+
+print(tokenizer.decode(output[0], skip_special_tokens=True))
