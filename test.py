44269415d539a3ee04d3b0680c9962a08a96af4e
badim41 <ramdomakk@gmail.com>
2023-10-06 07:22:07 +1200

    updated

--- test.py
+++ test.py
@@ -1,288 +1,193 @@
-# nums = []
-#
-#
-# async def count_variants(len):
-#     number_variants = 1
-#     for i in range(1 + len):
-#         if i < 1:
-#             continue
-#         number_variants *= i
-#         # print(number_variants)
-#     return number_variants
-#
-#
-# print(count_variants(16) * (8*12+5))
-import asyncio
-import configparser
-import multiprocessing
+# needed until the fork is merged upstream
+import sys;
+
+sys.path.insert(0, 'discord.py')
+
+import argparse
 import os
+import wave
 import time
-
-# print(124 / 36**6)
-# print(1 / 17554695.96774194)
-# num = int("1" + ("0" * 24))
-# print(len(str(num)))
-#
-# num_result = []
-# while num / 16 >= 1:
-#     num_result.append(str(int(num / 16)))
-#     num -= 16 * int(num / 16)
-# num_result.append(str(num))
-# num = ''.join(num_result)
-# print(num)
-# print(len(num))
-spokenText = "протокол 13 -url https://www.youtube.com/watch?v=TFtjM6piFPY"
-config = configparser.ConfigParser()
+import random
+import asyncio
+import numpy as np
+import discord
+import grpc
+import google.oauth2.service_account
+import google.cloud.speech_v1
+import google.cloud.speech_v1.gapic.transports.speech_grpc_transport
 
 
-async def set_config_static_values(key, value):
-    config.read('config.ini')
-    config.set('Values', key, value)
-    # Сохранение
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
+class GoogleSpeechToText:
+    def __init__(self, endpoint, lang, recognition_model, api_credentials=None):
+        credentials = google.oauth2.service_account.Credentials.from_service_account_file(
+            api_credentials) if api_credentials else grpc.local_channel_credentials()
+        LocalSpeechGrpcTransport = type('LocalSpeechGrpcTransport', (
+        google.cloud.speech_v1.gapic.transports.speech_grpc_transport.SpeechGrpcTransport,), dict(
+            create_channel=lambda self, address, credentials, **kwargs: grpc.secure_channel(address, credentials,
+                                                                                            **kwargs)))
+        client_options = dict(api_endpoint=endpoint)
+
+        self.client = google.cloud.speech_v1.SpeechClient(credentials=credentials,
+                                                          client_options=client_options) if api_credentials else google.cloud.speech_v1.SpeechClient(
+            transport=LocalSpeechGrpcTransport(address=endpoint, credentials=credentials),
+            client_options=client_options)
+        self.lang = lang
+        self.recognition_model = recognition_model
+
+    def transcribe(self, pcm_s16le, sample_rate, num_channels):
+        res = self.client.recognize(
+            dict(audio_channel_count=num_channels, encoding='LINEAR16', sample_rate_hertz=sample_rate,
+                 language_code=self.lang, model=self.recognition_model), dict(content=pcm_s16le))
+        hyp = res.results[0].alternatives[0].transcript if len(res.results) > 0 else ''
+        return hyp
 
 
-async def createAICaver(ctx):
-    global spokenText
-    message = spokenText
-    lines = message.split("\n")
-    if not os.path.exists("caversAI/audio_links.txt"):
-        with open("caversAI/audio_links.txt", "w"):
-            pass
+class DebugDumpRawAudio:
+    def __init__(self, debug_dir):
+        os.makedirs(debug_dir, exist_ok=True)
+        self.debug_dir = debug_dir
 
-    with open("caversAI/audio_links.txt", "a") as writer:
-        for line in lines:
-            writer.write(line + "\n")
-        task1 = asyncio.create_task(prepare_audio_process_cuda_0(ctx))
-        task2 = asyncio.create_task(play_audio_process(ctx))
-
-        # Ждем завершения обоих задач
-        await task1
-        await task2
+    def transcribe(self, pcm_s16le, sample_rate, num_channels):
+        audio_path = os.path.join(self.debug_dir,
+                                  f'{int(time.time())}.{random.randint(1000, 9999)}._s16le_hz{sample_rate}_ch{num_channels}.wav')
+        with wave.open(audio_path, 'wb') as w:
+            w.setnchannels(num_channels)
+            w.setsampwidth(16 // 8)
+            w.setframerate(sample_rate)
+            w.writeframes(pcm_s16le)
 
 
-async def prepare_audio_process_cuda_0(ctx):
-    while True:
-        try:
-            with open("caversAI/audio_links.txt") as reader:
-                line = reader.readline()
-                if not line == "" and not line is None:
-                    # youtube_dl_path = "youtube-dl.exe"
-                    if "https://youtu.be/" not in line and "https://www.youtube.com/" not in line:
-                        print("Ссылка не с YT")
-                        await remove_line_from_txt("caversAI/audio_links.txt", 1)
-                        break
+class BufferAudioSink(discord.AudioSink):
+    def __init__(self, flush):
+        self.flush = flush
+        self.NUM_CHANNELS = discord.opus.Decoder.CHANNELS
+        self.NUM_SAMPLES = discord.opus.Decoder.SAMPLES_PER_FRAME
+        self.SAMPLE_RATE_HZ = discord.opus.Decoder.SAMPLING_RATE
+        self.BUFFER_FRAME_COUNT = 500
+        self.buffer = np.zeros(shape=(self.NUM_SAMPLES * self.BUFFER_FRAME_COUNT, self.NUM_CHANNELS), dtype='int16')
+        self.buffer_pointer = 0
+        self.speaker = None
 
-                    # url = line[line.index("https://"):].split()[0]
-                    # if " " in url:
-                    #     url = url[:url.index(" ")]
-
-                    # command = f"{youtube_dl_path} {url} --max-filesize {video_length * 2 + 2}m --min-views 50000 --no-playlist --buffer-size 8K"
-                    # if console_command_runner(command, ctx):
-                    #     print("Условия выполнены")
-                    # else:
-                    #     print("Условия не выполнены")
-                    #     await remove_line_from_txt("caversAI/audio_links.txt", 1)
-                    #     break
-
-                    params = await getCaverPrms(line, ctx)
-                    params += " -cuda 0"
-                    await remove_line_from_txt("caversAI/audio_links.txt", 1)
-                    print("запуск AICoverGen")
-                    print(params, ctx)
-                    time.sleep(5)
-                    lines = []
-                    with open("caversAI/queue.txt", "r") as reader:
-                        lines = reader.readlines()
-                    lines.append("path/path/file.mp3 -start 0 -time -1\n")
-                    with open("caversAI/queue.txt", "w") as writer:
-                        writer.writelines(lines)
-                else:
-                    print("Больше нет ссылок")
-                    await set_config_static_values("queue", "False")
-                    break
-        except (IOError, KeyboardInterrupt):
-            pass
-
-
-async def remove_line_from_txt(file_path, delete_line):
-    try:
-        if not os.path.exists(file_path):
-            print(f"File not found: {file_path}")
+    def write(self, voice_data):
+        if voice_data.user is None:
             return
+        speaker = voice_data.user.id
+        frame = np.ndarray(shape=(self.NUM_SAMPLES, self.NUM_CHANNELS), dtype=self.buffer.dtype, buffer=voice_data.data)
+        speaking = np.abs(frame).sum() > 0
+        need_flush = (self.buffer_pointer >= self.BUFFER_FRAME_COUNT - 2) or (
+                    not speaking and self.buffer_pointer > 0.5 * self.BUFFER_FRAME_COUNT)  # or (self.speaker is not None and speaker != self.speaker)
 
-        lines = []
-        with open(file_path, "r") as reader:
-            i = 1
-            for line in reader:
-                if i == delete_line:
-                    print(f"Line removed: {line}")
-                else:
-                    lines.append(line)
-                i += 1
+        if speaking:
+            self.buffer[
+            (self.buffer_pointer * self.NUM_SAMPLES): ((1 + self.buffer_pointer) * self.NUM_SAMPLES)] = frame
+            self.buffer_pointer += 1
+            self.speaker = speaker
 
-        with open(file_path, "w") as writer:
-            for line in lines:
-                writer.write(line)
-    except IOError as e:
-        print(e)
-
-
-async def getCaverPrms(line, ctx):
-    # SONG_INPUT
-    url = "."
-    if "-url" in line:
-        if "https" in line:
-            url = line[line.index("https"):]
-            if " " in url:
-                url = url[:url.index(" ")]
-
-    # RVC_DIRNAME
-
-    # PITCH_CHANGE
-    pitch = 0
-    if "-pitch" in line:
-        pitch = await extract_number_after_keyword(line, "-pitch")
-        if pitch < -2 or pitch > 2:
-            pitch = 0
-
-    # время (не является аргументом для RVC)
-    time = -1
-    if "-time" in line:
-        time = await extract_number_after_keyword(line, "-time")
-        if time < 0:
-            time = -1
-
-    # INDEX_RATE
-    indexrate = 0.5
-    if "-indexrate" in line:
-        indexrate = await extract_double_after_keyword(line, "-indexrate")
-        if indexrate < 0 or indexrate > 1:
-            indexrate = 0.5
-
-    # RMS_MIX_RATE
-    loudness = 0.2
-    if "-loudness" in line:
-        loudness = await extract_double_after_keyword(line, "-loudness")
-        if loudness < 0 or loudness > 1:
-            loudness = 0.5
-
-    # MAIN_VOCALS_VOLUME_CHANGE
-    mainVocal = 0
-    if "-vocal" in line:
-        mainVocal = await extract_number_after_keyword(line, "-vocal")
-        if mainVocal < -20 or mainVocal > 0:
-            mainVocal = 0
-
-    # BACKUP_VOCALS_VOLUME_CHANGE
-    backVocal = 0
-    if "-bvocal" in line:
-        backVocal = await extract_number_after_keyword(line, "-bvocal")
-        if backVocal < -20 or backVocal > 0:
-            backVocal = 0
-
-    # INSTRUMENTAL_VOLUME_CHANGE
-    music = 0
-    if "-music" in line:
-        music = await extract_number_after_keyword(line, "-music")
-        if music < -20 or music > 0:
-            music = 0
-
-    # REVERB_SIZE
-    roomsize = 0.2
-    if "-roomsize" in line:
-        roomsize = await extract_double_after_keyword(line, "-roomsize")
-        if roomsize < 0 or roomsize > 1:
-            roomsize = 0.2
-
-    # REVERB_WETNESS
-    wetness = 0.1
-    if "-wetness" in line:
-        wetness = await extract_double_after_keyword(line, "-wetness")
-        if wetness < 0 or wetness > 1:
-            wetness = 0.1
-
-    # REVERB_DRYNESS
-    dryness = 0.85
-    if "-dryness" in line:
-        dryness = await extract_double_after_keyword(line, "-dryness")
-        if dryness < 0 or dryness > 1:
-            dryness = 0.85
-
-    # начало
-    start = 0
-    if "-start" in line:
-        start = await extract_number_after_keyword(line, "-start")
-        if start < 0:
-            start = 0
-
-    outputFormat = "mp3"
-
-    return f"python src/main.py -i {url} -dir modelsRVC/voice -p {pitch} -ir {indexrate} -rms {loudness} -mv {mainVocal} -bv {backVocal} -iv {music} -rsize {roomsize} -rwet {wetness} -rdry {dryness} -start {start} -time {time} -oformat {outputFormat}"
+        if need_flush:
+            pcm_s16le = self.buffer.tobytes()
+            self.buffer.fill(0)
+            self.buffer_pointer = 0
+            self.flush(self.speaker, pcm_s16le, self.SAMPLE_RATE_HZ, self.NUM_CHANNELS)
 
 
-async def play_audio_process(ctx):
-    await set_config_static_values("queue", "True")
-    while True:
-        with open("caversAI/queue.txt", "r") as reader:
-            line = reader.readline()
-            if not line == "" and not line is None:
-                print("Playing: " + line)
-                print("2")
-                params = await getCaverPrms(line, ctx)
-                time = await extract_number_after_keyword(params, "-time")
-                stop_milliseconds = await extract_number_after_keyword(params, "-start")
-                audio_path = line.split()[0]
-                await playSoundFile(audio_path, time, stop_milliseconds, ctx)
-                await remove_line_from_txt("caversAI/queue.txt", 1)
-            else:
-                config.read('config.ini')
-                continue_process = config.getboolean('Values', 'queue')
-                if not continue_process:
-                    print("==============================================")
-                    break
+class DiscordBotClient(discord.Client):
+    def __init__(self, transcriber, text_channel_id=None, voice_channel_id=None, text_channel_name=None,
+                 voice_channel_name=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        discord.opus._load_default()
+
+        self.transcriber = transcriber
+        self.voice_client = None
+        self.text_channel = None
+        self.voice_channel = None
+        self.speakingUserString = ''
+        self.text_channel_id = text_channel_id
+        self.text_channel_name = text_channel_name
+        self.voice_channel_id = voice_channel_id
+        self.voice_channel_name = voice_channel_name
+        self.audioSink = BufferAudioSink(self.transcribe)
+        self.messages = []
+        self.loop.create_task(self.message_sending_loop())
+
+    def transcribe(self, speaker, pcm_s16le, sample_rate, num_channels):
+        hyp = self.transcriber.transcribe(pcm_s16le, sample_rate, num_channels)
+        print('Transcribing', '[', hyp, ']')
+        if hyp:
+            self.messages.append((speaker, hyp))
+
+    async def message_sending_loop(self, timeout=0.25):
+        while True:
+            if not self.guilds or not self.messages:
+                await asyncio.sleep(timeout)
+                continue
+
+            for speaker, hyp in self.messages:
+                member = self.guilds[0].get_member(speaker)
+                speaker_pretty = str(
+                    member.nick if member.nick is not None else member) if member is not None else 'N/A'
+                await self.text_channel.send(f'[{speaker_pretty}] says: [{hyp}]')
+
+            self.messages = []
+
+    async def on_ready(self):
+        print('\n'.join(map(repr, self.get_all_channels())))
+
+        self.text_channel = discord.utils.get(self.get_all_channels(), **(
+            dict(id=self.text_channel_id) if self.text_channel_id is not None else dict(name=self.text_channel_name)))
+        self.voice_channel = discord.utils.get(self.get_all_channels(), **(
+            dict(id=self.voice_channel_id) if self.voice_channel_id is not None else dict(
+                name=self.voice_channel_name)))
+
+        print('Logged in to Discord as {} - ID {}'.format(self.user.name, self.user.id))
+        print('Ready to recieve commands!')
+
+    async def on_message(self, message, timeout=1.0):
+        if message.author == self.user:
+            return
+        messageText = message.content.lower().strip()
+
+        if messageText == '!transcribeyes':
+            if self.voice_channel is not None:
+                self.voice_client = await self.voice_channel.connect()
+                self.voice_client.listen(self.audioSink)
+                print("Connected!")
+
+        elif messageText == '!transcribenot':
+            if self.voice_client is not None:
+                print("Attempting to hang up from voice channel")
+                try:
+                    self.voice_client.stop()
+                    self.voice_client.stop_listening()
+                except Exception as e:
+                    print(e)
+                time.sleep(timeout)
+                await self.voice_client.disconnect()
+                self.voice_client = None
+                print("Disconnected!")
 
 
-async def extract_number_after_keyword(input, keyword):
-    input = ''.join(char if char.isalnum() or char.isspace() else ' ' for char in input)
-    index = input.find(keyword)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--discord-bot-token', required=True)
+    parser.add_argument('--google-api-credentials-file', required=True)
+    parser.add_argument('--debug', help='debug dir')
+    parser.add_argument('--lang', default='ru-RU',
+                        help='see http://g.co/cloud/speech/docs/languages for a list of supported languages')
+    parser.add_argument('--recognition-model', default='phone_call',
+                        choices=['phone_call', 'default', 'video', 'command_and_search'])
+    parser.add_argument('--endpoint', default=google.cloud.speech_v1.SpeechClient.SERVICE_ADDRESS)
+    parser.add_argument('--text-channel-name', default='general')
+    parser.add_argument('--voice-channel-name', default='General')
+    parser.add_argument('--text-channel-id', type=int)
+    parser.add_argument('--voice-channel-id', type=int)
+    args = parser.parse_args()
 
-    if index != -1:
-        start = index + len(keyword) + 1
-        end = input.find(" ", start) if " " in input[start:] else len(input)
-        numberStr = ''.join(char for char in input[start:end] if char.isdigit())
+    discord_bot_token = args.discord_bot_token
 
-        if numberStr:
-            return int(numberStr)
-
-    return -1
-
-
-async def extract_double_after_keyword(input, keyword):
-    input = ''.join(char if char.isalnum() or char.isspace() else ' ' for char in input)
-    index = input.find(keyword)
-
-    if index != -1:
-        start = index + len(keyword) + 1
-        end = input.find(" ", start) if " " in input[start:] else len(input)
-        numberStr = ''.join(char for char in input[start:end] if char.isdigit() or char == '.')
-
-        try:
-            if numberStr:
-                return float(numberStr.replace(',', '.'))
-        except ValueError:
-            pass
-
-    return -1
-
-
-async def playSoundFile(audio_file_path, duration, start_seconds, ctx):
-    print("PLAYING:", ctx, audio_file_path, duration, start_seconds)
-    print("Аудио закончилось")
-
-
-if __name__ == "__main__":
-    asyncio.run(createAICaver("..."))
-
-
+    transcriber = GoogleSpeechToText(endpoint=args.endpoint, recognition_model=args.recognition_model, lang=args.lang,
+                                     api_credentials=args.google_api_credentials_file) if not args.debug else DebugDumpRawAudio(
+        args.debug)
+    bot = DiscordBotClient(text_channel_id=args.text_channel_id, voice_channel_id=args.voice_channel_id,
+                           text_channel_name=args.text_channel_name, voice_channel_name=args.voice_channel_name,
+                           transcriber=transcriber)
+    bot.run(discord_bot_token)
