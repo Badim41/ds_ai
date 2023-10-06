78718a4369bbccd95e4a382c5422dad14fd0a93b
badim41 <ramdomakk@gmail.com>
2023-10-07 11:49:33 +1200

    updated

--- image_create.py
+++ image_create.py
@@ -1,15 +1,12 @@
 # !git clone https://github.com/ai-forever/diffusers.git
 import argparse
 import random
-import sys
 from diffusers import KandinskyV22Pipeline, KandinskyV22PriorPipeline
-import torch
 import torch
 from diffusers.utils import load_image
 from torchvision import transforms
 from transformers import CLIPVisionModelWithProjection
 from diffusers.models import UNet2DConditionModel
-import numpy as np
 import cv2
 
 def generate_picture(prompt, negative_prompt, x, y, steps, seed):
@@ -17,20 +14,24 @@ def generate_picture(prompt, negative_prompt, x, y, steps, seed):
         'kandinsky-community/kandinsky-2-2-prior',
         subfolder='image_encoder'
     ).half().to("cuda:0")
+    print("image3")
     unet = UNet2DConditionModel.from_pretrained(
         'kandinsky-community/kandinsky-2-2-decoder',
         subfolder='unet'
     ).half().to("cuda:0")
+    print("image4")
     prior = KandinskyV22PriorPipeline.from_pretrained(
         'kandinsky-community/kandinsky-2-2-prior',
         image_encoder=image_encoder,
         torch_dtype=torch.float16
     ).to("cuda:0")
+    print("image5")
     decoder = KandinskyV22Pipeline.from_pretrained(
         'kandinsky-community/kandinsky-2-2-decoder',
         unet=unet,
         torch_dtype=torch.float16
     ).to("cuda:0")
+    print("image6")
     torch.manual_seed(seed)
 
     negative_prior_prompt = negative_prompt
@@ -39,12 +40,14 @@ def generate_picture(prompt, negative_prompt, x, y, steps, seed):
         num_inference_steps=steps,
         num_images_per_prompt=1
     )
+    print("image7")
 
     negative_emb = prior(
         prompt=negative_prior_prompt,
         num_inference_steps=steps,
         num_images_per_prompt=1
     )
+    print("image8")
 
     images = decoder(
         image_embeds=img_emb.image_embeds,
@@ -52,6 +55,7 @@ def generate_picture(prompt, negative_prompt, x, y, steps, seed):
         num_inference_steps=75,
         height=y,
         width=x)
+    print("image9")
     cv2.imwrite("picture.png", images.images[0])
     print("Изображение сохранено как picture.png")
 
@@ -71,5 +75,7 @@ if __name__ == '__main__':
                         help='steps')
     parser.add_argument('-seed', '--seed', type=int, default=random.randint(1, 10000),
                        help='seed')
+    print("image1")
     args = parser.parse_args()
+    print("image2")
     generate_picture(args.prompt, args.negative_prompt, args.x, args.y, args.steps, args.seed)
