8c9afd6152c86be543b4307c62430d8226dee819
badim41 <ramdomakk@gmail.com>
2023-10-13 22:29:38 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -440,7 +440,7 @@ async def __cover(
         ctx,
         url: Option(str, description='Ссылка на видео', required=False),
         audio_path: Option(discord.SlashCommandOptionType.attachment, description='Аудиофайл',
-                           required=False),
+                           required=False, default=None),
         voice: Option(str, description='Голос для видео', required=False, default=None),
         pitch: Option(str, description='Кто говорит/поёт в видео?', required=False,
                       choices=['мужчина', 'женщина'], default=None),
