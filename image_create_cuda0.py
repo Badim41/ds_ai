5f942dc534dc7fc85141b458f6d7a4de484c1fda
badim41 <ramdomakk@gmail.com>
2023-10-14 12:36:27 +1200

    updated

--- image_create_cuda0.py
+++ image_create_cuda0.py
@@ -69,66 +69,69 @@ def generate_picture0():
     set_get_config("model_loaded", True)
     # loop update image prompt
     while True:
-        print("check prompt0")
-        prompt = set_get_config("prompt")
-        if prompt == "None":
-            time.sleep(10)
-            continue
-        set_get_config("prompt", "None")
-        start_time = datetime.datetime.now()
-        current_time = start_time.time()
-        print("Начало:", current_time)
+        try:
+            print("check prompt0")
+            prompt = set_get_config("prompt")
+            if prompt == "None":
+                time.sleep(10)
+                continue
+            set_get_config("prompt", "None")
+            start_time = datetime.datetime.now()
+            current_time = start_time.time()
+            print("Начало:", current_time)
 
-        negative_prompt = set_get_config("negative_prompt")
-        x = int(set_get_config("x"))
-        y = int(set_get_config("y"))
-        steps = int(set_get_config("steps"))
-        seed = int(set_get_config("seed"))
-        strength = float(set_get_config("strength"))
-        strength_prompt = float(set_get_config("strength_prompt"))
-        strength_negative_prompt = float(set_get_config("strength_negative_prompt"))
-        image_name = set_get_config("input")
-        # create pipes
-        print("image_generate1")
-        pipe_prior = pipe_prior.to("cuda:0")
-        pipe = pipe.to("cuda:0")
-        print("image_generate2")
+            negative_prompt = set_get_config("negative_prompt")
+            x = int(set_get_config("x"))
+            y = int(set_get_config("y"))
+            steps = int(set_get_config("steps"))
+            seed = int(set_get_config("seed"))
+            strength = float(set_get_config("strength"))
+            strength_prompt = float(set_get_config("strength_prompt"))
+            strength_negative_prompt = float(set_get_config("strength_negative_prompt"))
+            image_name = set_get_config("input")
+            # create pipes
+            print("image_generate1")
+            pipe_prior = pipe_prior.to("cuda")
+            pipe = pipe.to("cuda")
+            print("image_generate2")
 
-        # create generator
-        generator = torch.Generator(device="cuda:0").manual_seed(seed)
-        print("image_generate3")
+            # create generator
+            generator = torch.Generator(device="cuda").manual_seed(seed)
+            print("image_generate3")
 
-        # make hint
-        img = load_image(image_name).resize((x, y))
-        depth_estimator = pipeline("depth-estimation")
-        hint = make_hint(img, depth_estimator).unsqueeze(0).half().to("cuda:0")
-        print("image_generate4")
+            # make hint
+            img = load_image(image_name).resize((x, y))
+            depth_estimator = pipeline("depth-estimation")
+            hint = make_hint(img, depth_estimator).unsqueeze(0).half().to("cuda")
+            print("image_generate4")
 
-        # run prior pipeline
-        img_emb = pipe_prior(prompt=prompt, image=img, strength=strength_prompt, generator=generator)
-        negative_emb = pipe_prior(prompt=negative_prompt, image=img, strength=strength_negative_prompt,
-                                  generator=generator)
-        print("image_generate5")
-        # run controlnet img2img pipeline
-        images = pipe(
-            image=img,
-            strength=strength,
-            image_embeds=img_emb.image_embeds,
-            negative_image_embeds=negative_emb.image_embeds,
-            hint=hint,
-            num_inference_steps=steps,
-            generator=generator,
-            height=y,
-            width=x,
-        ).images
+            # run prior pipeline
+            img_emb = pipe_prior(prompt=prompt, image=img, strength=strength_prompt, generator=generator)
+            negative_emb = pipe_prior(prompt=negative_prompt, image=img, strength=strength_negative_prompt,
+                                      generator=generator)
+            print("image_generate5")
+            # run controlnet img2img pipeline
+            images = pipe(
+                image=img,
+                strength=strength,
+                image_embeds=img_emb.image_embeds,
+                negative_image_embeds=negative_emb.image_embeds,
+                hint=hint,
+                num_inference_steps=steps,
+                generator=generator,
+                height=y,
+                width=x,
+            ).images
 
-        images[0].save(image_name)
+            images[0].save(image_name)
 
-        end_time = datetime.datetime.now()
-        current_time = end_time.time()
-        print("Конец:", current_time)
-        
-        spent_time = end_time - start_time
-        print("Прошло времени:", spent_time)
-        set_get_config("spent_time", spent_time)
-        set_get_config("result", image_name)
+            end_time = datetime.datetime.now()
+            current_time = end_time.time()
+            print("Конец:", current_time)
+
+            spent_time = end_time - start_time
+            print("Прошло времени:", spent_time)
+            set_get_config("spent_time", spent_time)
+            set_get_config("result", image_name)
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
