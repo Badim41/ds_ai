c602be669c21acd90ec8a4dcd4a630d1a82bc8fd
badim41 <ramdomakk@gmail.com>
2023-10-19 15:55:29 +1200

    updated

--- function.py
+++ function.py
@@ -287,9 +287,9 @@ async def translate(text):
 
 async def one_gpt_run(provider, prompt):
     gpt_model = "gpt-3.5-turbo"
-    if "Llama2" in provider:
+    if "Llama2" in str(provider):
         gpt_model = "gpt-3.5"
-    if "GeekGpt" in provider:
+    if "GeekGpt" in str(provider):
         gpt_model = "gpt-4"
     result = await g4f.ChatCompletion.create_async(
         model=gpt_model,
