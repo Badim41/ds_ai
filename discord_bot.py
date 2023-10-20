56c0ebe8ad1f5be66d2819549fbae3bcd63287f8
badim41 <ramdomakk@gmail.com>
2023-10-20 16:09:07 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -94,6 +94,7 @@ async def on_message(message):
             await run_main_with_settings(ctx, text, True)
         except Exception as e:
             await ctx.send(f"Ошибка при команде say с параметрами {message}: {e}")
+    await bot.process_commands(message)
 
 
 @bot.slash_command(name="change_video",
@@ -696,11 +697,6 @@ async def command_line(ctx, *args):
     except Exception as e:
         await ctx.send(f"Произошла неизвестная ошибка: {e}")
 
-@bot.command(aliases=['check'], help="вывод 10 сообщений из истории чата")
-async def check_messages(ctx):
-    print("histoty")
-    async for message in ctx.channel.history(limit=10):
-        print(f"Сообщение от {message.author.name}: {message.content}")
 
 
 async def run_main_with_settings(ctx, spokenText, writeAnswer):
