fb2986169a9dc02917433069e19c08611d20b5c7
badim41 <ramdomakk@gmail.com>
2023-10-21 16:45:50 +1200

    updated

--- function.py
+++ function.py
@@ -1362,8 +1362,8 @@ async def preprocess_text(text):
 async def extract_number_after_keyword(input, keyword):
     index = input.find(keyword)
     if index != -1:
-        start = index + len(keyword)
-        remaining_str = input[start:].strip()
+        remaining_str = input[index + len(keyword) + 1:]
+        remaining_str = remaining_str[:remaining_str.find(" ")]
         if remaining_str:
             if remaining_str[0] == '-':
                 numberStr = '-' + ''.join(char for char in remaining_str[1:] if char.isdigit())
