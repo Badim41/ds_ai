ad5f59bae50203979bda91da00b4406628fcc69f
badim41 <ramdomakk@gmail.com>
2023-10-22 09:26:35 +1200

    updated

--- function.py
+++ function.py
@@ -1000,7 +1000,7 @@ async def run_ai_cover_gen(line, ctx, wait=False):
     print(
         f"python src/main.py -i \"{url}\" -dir {voice} -p \"{pitch}\" -ir {indexrate} -rms {loudness} -mv {mainVocal} -bv {backVocal} -iv {music} -rsize {roomsize} -rwet {wetness} -rdry {dryness} -start {start} -time {time} -oformat {outputFormat} -output {output}")
 
-    from src.main import run_ai_cover_gen
+    from main import run_ai_cover_gen
     loop = asyncio.get_event_loop()
     # song_input, rvc_dirname, pitch, keep_files=False, index_rate=0.5, filter_radius=3, rms_mix_rate=0.25,
     #         pitch_detection_algo='rmvpe', crepe_hop_length=128, protect=0.33, main_vol=0, backup_vol=0, inst_vol=0,
--- src/README.md
+++
@@ -1,2 +0,0 @@
-Ссылка на оригинальный репозиторий:
-https://github.com/SociallyIneptWeeb/AICoverGen
