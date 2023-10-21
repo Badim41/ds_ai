c84a64cc076251160b2d81893ae002e391614c73
badim41 <ramdomakk@gmail.com>
2023-10-21 17:00:17 +1200

    updated

--- test.py
+++ test.py
@@ -1,19 +1,2 @@
-
-def extract_number_after_keyword(input, keyword):
-    index = input.find(keyword)
-    if index != -1:
-        remaining_str = input[index + len(keyword) + 1:]
-        remaining_str = remaining_str[:remaining_str.find(" ")]
-        if remaining_str:
-            if remaining_str[0] == '-':
-                numberStr = '-' + ''.join(char for char in remaining_str[1:] if char.isdigit())
-            else:
-                numberStr = ''.join(char for char in remaining_str if char.isdigit())
-            if numberStr:
-                print(f"Extract: {keyword}, Number:{numberStr}")
-                return int(numberStr)
-    return -1
-
-line = "-url 0 -pitch 0 -rsize 1"
-pitch = extract_number_after_keyword(line, "-pitch")
-print(pitch)
+str1 = "12l342"
+print(''.join(char for char in str1 if char.isdigit()))
