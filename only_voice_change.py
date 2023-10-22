1f5b8a54cf15ef7cfd5d2ac9b8aa451ee297db70
Vadim <115208665+Badim41@users.noreply.github.com>
2023-10-22 15:37:34 +1200

    Update only_voice_change.py

--- only_voice_change.py
+++ only_voice_change.py
@@ -4,7 +4,7 @@ import os
 
 from rvc import Config, load_hubert, get_vc, rvc_infer
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 rvc_models_dir = os.path.join(BASE_DIR, 'rvc_models')
 
 
