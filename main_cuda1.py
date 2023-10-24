7f050bb3a0df58379c66cfc653c439fbdf61e917
badim41 <ramdomakk@gmail.com>
2023-10-25 08:00:41 +1200

    updated

--- main_cuda1.py
+++ main_cuda1.py
@@ -258,9 +258,9 @@ def combine_audio(audio_paths, output_path, main_gain, backup_gain, inst_gain, o
     combined_audio.export(output_path, format=output_format)
 
     output_file = os.path.dirname(output_path) + "/combined.m4a"
-    print("M4A FILE:", main_gain, backup_gain, inst_gain, output_file, sep="|||")
+    print("M4A FILE:", audio_paths[0], audio_paths[1], audio_paths[2], output_file, sep="|||")
     ffmpeg_command = (
-        f'ffmpeg -i {main_gain} -i {backup_gain} -i {inst_gain} -filter_complex "[0:a][1:a]amerge=inputs=3[aout]" -map "[aout]" -c:a aac -strict experimental -q:a 1 {output_file} -y'
+        f'ffmpeg -i {audio_paths[0]} -i {audio_paths[1]} -i {audio_paths[2]} -filter_complex "[0:a][1:a]amerge=inputs=3[aout]" -map "[aout]" -c:a aac -strict experimental -q:a 1 {output_file} -y'
     )
     subprocess.run(ffmpeg_command, shell=True)
 
