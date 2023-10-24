8b4073f569441e05725f6712cdac26768b0bacb3
badim41 <ramdomakk@gmail.com>
2023-10-24 16:21:28 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -729,7 +729,8 @@ async def __cover(
         spent_time = str(end_time - start_time)
         # убираем миллисекунды
         spent_time = spent_time[:spent_time.find(".")]
-        await ctx.send("Потрачено на обработку:" + spent_time)
+        if not "0:00:00" in str(spent_time):
+            await ctx.send("Потрачено на обработку:" + spent_time)
     except Exception as e:
         await ctx.send(f"Ошибка при изменении голоса (с параметрами {param_string}): {e}")
 
