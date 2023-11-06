82bfda48162b4301a521e3c506aed4b781582bd0
badim41 <ramdomakk@gmail.com>
2023-11-07 11:24:00 +1200

    updated

--- set_get_config.py
+++ set_get_config.py
@@ -6,12 +6,12 @@ config = configparser.ConfigParser()
 
 async def set_get_config_all(section, key, value=None, error=0):
     try:
-        config.read('config.ini', encoding='utf-8')
+        config.read(f'configs/{section}.ini', encoding='utf-8')
         if value is None:
-            return config.get(section, key)
-        config.set(section, key, str(value))
+            return config.get("Default", key)
+        config.set("Default", key, str(value))
         # Сохранение
-        with open('config.ini', 'w', encoding='utf-8') as configfile:
+        with open(f'configs/{section}.ini', 'w', encoding='utf-8') as configfile:
             config.write(configfile)
     except Exception as e:
         if error == 5:
@@ -21,14 +21,13 @@ async def set_get_config_all(section, key, value=None, error=0):
         return result
 
 def set_get_config_all_not_async(section, key, value=None, error=0):
-
     try:
-        config.read('config.ini', encoding='utf-8')
+        config.read(f'configs/{section}.ini', encoding='utf-8')
         if value is None:
-            return config.get(section, key)
-        config.set(section, key, str(value))
+            return config.get("Default", key)
+        config.set("Default", key, str(value))
         # Сохранение
-        with open('config.ini', 'w', encoding='utf-8') as configfile:
+        with open(f'configs/{section}.ini', 'w', encoding='utf-8') as configfile:
             config.write(configfile)
     except Exception as e:
         if error == 5:
