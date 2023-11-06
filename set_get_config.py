5404d01816dc9b75e9e389b89aacb413bffb050c
badim41 <ramdomakk@gmail.com>
2023-11-06 20:14:51 +1200

    updated

--- set_get_config.py
+++ set_get_config.py
@@ -1,19 +1,17 @@
 import asyncio
 import configparser
-import threading
 import time
 
 config = configparser.ConfigParser()
-config_lock = threading.Lock()
 
 async def set_get_config_all(section, key, value=None, error=0):
     try:
-        config.read('config.ini')
+        config.read('config.ini', encoding='utf-8')
         if value is None:
             return config.get(section, key)
         config.set(section, key, str(value))
         # Сохранение
-        with open('config.ini', 'w') as configfile:
+        with open('config.ini', 'w', encoding='utf-8') as configfile:
             config.write(configfile)
     except Exception as e:
         if error == 5:
@@ -23,14 +21,15 @@ async def set_get_config_all(section, key, value=None, error=0):
         return result
 
 def set_get_config_all_not_async(section, key, value=None, error=0):
+
     try:
-        with config_lock:
-            config.read('config.ini')
-            if value is None:
-                return config.get(section, key)
-            config.set(section, key, str(value))
-            with open('config.ini', 'w') as configfile:
-                config.write(configfile)
+        config.read('config.ini', encoding='utf-8')
+        if value is None:
+            return config.get(section, key)
+        config.set(section, key, str(value))
+        # Сохранение
+        with open('config.ini', 'w', encoding='utf-8') as configfile:
+            config.write(configfile)
     except Exception as e:
         if error == 5:
             raise f"Ошибка при чтении конфига со значениями: {section}, {key}, {value}.\n{e}"
