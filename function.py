e1b7bc8ea01e9c593dbdd89db8a8492b4c3f0748
badim41 <ramdomakk@gmail.com>
2023-11-11 10:48:02 +1200

    updated

--- function.py
+++ function.py
@@ -1307,12 +1307,7 @@ async def speed_up_audio(input_file, speed_factor):
         sr_new = 22050
         y_resampled = librosa.resample(y, sr, sr_new)
         y_slowed = librosa.effects.time_stretch(y_resampled, rate=speed_factor)
-        output_dir = os.path.basename(input_file)
         sf.write(input_file, y_slowed, sr_new)
-
-        y, sr = sf.read(input_file)
-        y_stretch = pyrb.time_stretch(y, sr, speed_factor)
-        sf.write(output_dir + "output2.mp3", y_stretch, sr)
     else:
         sped_up_audio = audio.speedup(playback_speed=speed_factor)
         sped_up_audio.export(input_file, format="mp3")
