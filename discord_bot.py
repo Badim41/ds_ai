d1e6ecaceb58f5f6178dd9cfd216edf13280070a
badim41 <ramdomakk@gmail.com>
2023-11-23 12:05:03 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -1067,6 +1067,8 @@ async def agrs_with_txt(txt_file):
         voice_model = []
         for line in lines:
             if line.strip():
+                # забейте, просто нужен пробел и всё
+                line += " "
                 line = line.replace(": ", ":")
                 # /add_voice url:url_to_model name:some_name gender:мужчина info:some_info speed:some_speed voice_model:some_model
                 pattern = r'(\w+):(.+?)\s(?=\w+:|$)'
@@ -1076,7 +1078,7 @@ async def agrs_with_txt(txt_file):
 
                 url.append(arguments.get('url', None))
                 name.append(arguments.get('name', None))
-                gender.append(arguments.get('gender', None))
+                gender.append(arguments.get('gender', "мужчина"))
                 info.append(arguments.get('info', "Отсутствует"))
                 speed.append(arguments.get('speed', "1"))
                 voice_model.append(arguments.get('voice_model', "James"))
