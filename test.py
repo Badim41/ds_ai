be002e70f93792c6e8cf0f43698f718c5b8a6ae6
badim41 <ramdomakk@gmail.com>
2023-11-26 13:10:35 +1200

    updated

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+import sys
 
+from scipy.io.wavfile import write as write_wav
+from bark import SAMPLE_RATE, generate_audio, preload_models
+
+if __name__ == "__main__":
+    preload_models()
+    arguments = sys.argv
+    if len(arguments) > 2:
+        text = arguments[2]
+        audio_array = generate_audio(text)
+        write_wav("audio.wav", SAMPLE_RATE, audio_array)
