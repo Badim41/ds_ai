58a6271fe7622f3715c049d42ca90ffac594a6e6
badim41 <ramdomakk@gmail.com>
2023-11-26 13:14:47 +1200

    updated

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ from bark import SAMPLE_RATE, generate_audio, preload_models
 if __name__ == "__main__":
     preload_models()
     arguments = sys.argv
-    if len(arguments) > 2:
+    if len(arguments) > 3:
         text = arguments[2]
-        audio_array = generate_audio(text)
+        speaker = arguments[3]
+        audio_array = generate_audio(text, history_prompt=speaker)
         write_wav("audio.wav", SAMPLE_RATE, audio_array)
