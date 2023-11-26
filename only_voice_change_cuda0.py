98861b53cf9a854321d005466e2d386cdcb4c256
badim41 <ramdomakk@gmail.com>
2023-11-26 17:40:57 +1200

    updated

--- only_voice_change_cuda0.py
+++ only_voice_change_cuda0.py
@@ -27,7 +27,7 @@ def voice_change0():
                 continue
 
             if not voice_model == new_voice_model:
-                print("reload voice")
+                print("reload voice", new_voice_model)
                 if hubert_model and cpt:
                     del hubert_model, cpt
                 voice_model = new_voice_model
