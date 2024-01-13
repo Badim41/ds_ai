c85039bb235285be01a1a41d2449766156a3b0b7
badim41 <ramdomakk@gmail.com>
2024-01-13 16:35:31 +1200

    updated

--- test.py
+++ test.py
@@ -9,12 +9,14 @@ from discord import option, Option
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='\\', intents=intents)
 
-@bot.slash_command(name="channel")
-async def select_channel(
-    ctx: discord.ApplicationContext,
-    channel: Option(str, description='Нужная вам команда', required=True)
-):
-    await ctx.respond(f"Hi! You selected {channel.mention} channel.")
+@bot.slash_command(name='test_slash_command', description='Отвечает "Успешный тест!"')
+async def __test(ctx):
+    await ctx.respond('Успешный тест!')
+
+@bot.slash_command(name='test_del_command', description='Удаляет контекст и выводит сообщение "Успешный тест!"')
+async def __test(ctx):
+    await ctx.delete()
+    await ctx.send('Успешный тест!')
 
 if __name__ == "__main__":
     arguments = sys.argv
