d8d2522660a18758ae2e05059ed717b0e0004fd5
badim41 <ramdomakk@gmail.com>
2024-01-13 16:57:53 +1200

    updated

--- test.py
+++ test.py
@@ -1,31 +1,72 @@
 import asyncio
 import sys
 
-import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
+from typing import Union
 
-bot = commands.Bot()
+import discord
+from discord import option
 
+bot = discord.Bot()
 
-@bot.slash_command()
-async def choose_a_number(
-    interaction: Interaction,
-    number: int = SlashOption(
-        name="picker",
-        description="The number you want",
-        choices={"one": 1, "two": 2, "three": 3},
-    ),
-):
-    await interaction.response.send_message(f"You chose {number}!")
+# If you use commands.Bot, @bot.slash_command should be used for
+# slash commands. You can use @bot.slash_command with discord.Bot as well.
 
 
 @bot.slash_command()
-async def hi(
-    interaction: Interaction,
-    member: nextcord.Member = SlashOption(name="user", description="User to say hi to"),
+@option("name", description="Enter your name")
+@option("gender", description="Choose your gender", choices=["Male", "Female", "Other"])
+@option(
+    "age",
+    description="Enter your age",
+    min_value=1,
+    max_value=99,
+    default=18,
+    # Passing the default value makes an argument optional.
+    # You also can create optional arguments using:
+    # age: Option(int, "Enter your age") = 18
+)
+async def hello(
+    ctx: discord.ApplicationContext,
+    name: str,
+    gender: str,
+    age: int,
 ):
-    await interaction.response.send_message(f"{interaction.user} just said hi to {member.mention}")
+    await ctx.respond(
+        f"Hello {name}! Your gender is {gender} and you are {age} years old."
+    )
+
+
+@bot.slash_command(name="channel")
+@option(
+    "channel",
+    Union[discord.TextChannel, discord.VoiceChannel],
+    # You can specify allowed channel types by passing a union of them like this.
+    description="Select a channel",
+)
+async def select_channel(
+    ctx: discord.ApplicationContext,
+    channel: Union[discord.TextChannel, discord.VoiceChannel],
+):
+    await ctx.respond(f"Hi! You selected {channel.mention} channel.")
+
+
+@bot.slash_command(name="attach_file")
+@option(
+    "attachment",
+    discord.Attachment,
+    description="A file to attach to the message",
+    required=False,  # The default value will be None if the user doesn't provide a file.
+)
+async def say(
+    ctx: discord.ApplicationContext,
+    attachment: discord.Attachment,
+):
+    """This demonstrates how to attach a file with a slash command."""
+    if attachment:
+        file = await attachment.to_file()
+        await ctx.respond("Here's your file!", file=file)
+    else:
+        await ctx.respond("You didn't give me a file to reply with! :sob:")
 
 if __name__ == "__main__":
     arguments = sys.argv
