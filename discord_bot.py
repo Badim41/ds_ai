0c45d6a8504231e4b30a65965d9c3f0c2741189a
badim41 <ramdomakk@gmail.com>
2024-01-13 16:52:57 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -43,8 +43,8 @@ connections = {}
 
 stream_sink = StreamSink()
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='\\', intents=intents)
+# intents = discord.Intents.all()
+bot = commands.Bot()
 
 
 @bot.event
