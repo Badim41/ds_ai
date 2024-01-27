4afa1a63af02391dfa11c62bba643fd177033dde
badim41 <ramdomakk@gmail.com>
2024-01-27 20:49:23 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -1197,12 +1197,14 @@ async def __dialog(
         await set_get_config_all("dialog", "dialog", "True")
         await set_get_config_all("gpt", "gpt_mode", "None")
         # names, theme, infos, prompt, ctx
-        # запустим сразу 8 процессов для обработки голоса
+        # запустим сразу 4 процессов для обработки голоса
         await asyncio.gather(gpt_dialog(names, theme, infos, prompt, ctx), play_dialog(ctx),
                              create_audio_dialog(ctx, 0, "dialog"), create_audio_dialog(ctx, 1, "dialog"),
-                             create_audio_dialog(ctx, 2, "dialog"), create_audio_dialog(ctx, 3, "dialog"),
+                             create_audio_dialog(ctx, 2, "dialog"), create_audio_dialog(ctx, 3, "dialog"))
+        """ 
                              create_audio_dialog(ctx, 4, "dialog"), create_audio_dialog(ctx, 5, "dialog"),
-                             create_audio_dialog(ctx, 6, "dialog"), create_audio_dialog(ctx, 7, "dialog"))
+                             create_audio_dialog(ctx, 6, "dialog"), create_audio_dialog(ctx, 7, "dialog")
+                            """
     except Exception as e:
         traceback_str = traceback.format_exc()
         print(str(traceback_str))
