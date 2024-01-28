1277ca297f0fde97b837e9ca20beb7038346cd91
badim41 <ramdomakk@gmail.com>
2024-01-28 14:53:25 +1200

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -1679,9 +1679,9 @@ async def gpt_dialog(names, theme, infos, prompt_global, ctx):
                 while True:
                     with open("caversAI/dialog_create.txt", "r") as reader:
                         num_lines = len(reader.readlines())
-                    if num_lines > 1:
+                    if num_lines > 2:
                         await asyncio.sleep(3)
-                        print("wait, too many text > 1")
+                        print("wait, too many text > 2")
 
                         if await set_get_config_all("dialog", "dialog") == "False":
                             return
