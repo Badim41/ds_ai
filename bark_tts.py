da6a99517dcaebfc8a162c4a89d4cff808ea8d9e
badim41 <ramdomakk@gmail.com>
2024-02-10 15:42:16 +1200

    update

--- bark_tts.py
+++ bark_tts.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pydub import AudioSegment
+
 import nltk
 from bark import SAMPLE_RATE
 from bark.api import semantic_to_waveform
@@ -6,6 +8,7 @@ from bark.generation import (
     generate_text_semantic,
     preload_models
 )
+from scipy.io.wavfile import write as write_wav
 
 preload_models()
 
@@ -39,3 +42,14 @@ for sentence in sentences:
     audio_array = semantic_to_waveform(semantic_tokens, history_prompt=SPEAKER,)
     pieces += [audio_array, silence.copy()]
 
+# Concatenate audio pieces
+audio_result = np.concatenate(pieces)
+
+# Нормализация аудио до 16-бит
+audio_result = (audio_result / np.max(np.abs(audio_result)) * 32767).astype(np.int16)
+file_name = "result.wav"
+# Сохранение
+write_wav(file_name, SAMPLE_RATE, audio_result)
+
+audio = AudioSegment.from_wav(file_name)
+audio.export("1.mp3", format="mp3")
