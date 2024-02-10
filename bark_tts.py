3f3c1098f5aa044cbfb71650d5240fc35c8aae84
badim41 <ramdomakk@gmail.com>
2024-02-10 15:14:40 +1200

    update

---
+++ bark_tts.py
@@ -0,0 +1,41 @@
+import numpy as np
+import nltk
+from bark import SAMPLE_RATE
+from bark.api import semantic_to_waveform
+from bark.generation import (
+    generate_text_semantic,
+    preload_models
+)
+
+preload_models()
+
+script = """
+Hey, have you heard about this new text-to-audio model called "Bark"? 
+Apparently, it's the most realistic and natural-sounding text-to-audio model 
+out there right now. People are saying it sounds just like a real person speaking. 
+I think it uses advanced machine learning algorithms to analyze and understand the 
+nuances of human speech, and then replicates those nuances in its own speech output. 
+It's pretty impressive, and I bet it could be used for things like audiobooks or podcasts. 
+In fact, I heard that some publishers are already starting to use Bark to create audiobooks. 
+It would be like having your own personal voiceover artist. I really think Bark is going to 
+be a game-changer in the world of text-to-audio technology.
+""".replace("\n", " ").strip()
+
+sentences = nltk.sent_tokenize(script)
+
+GEN_TEMP = 0.6
+SPEAKER = "v2/ru_speaker_2"
+silence = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence
+
+pieces = []
+for sentence in sentences:
+    semantic_tokens = generate_text_semantic(
+        sentence,
+        history_prompt=SPEAKER,
+        temp=GEN_TEMP,
+        min_eos_p=0.05,  # this controls how likely the generation is to end
+    )
+
+    audio_array = semantic_to_waveform(semantic_tokens, history_prompt=SPEAKER,)
+    pieces += [audio_array, silence.copy()]
+
