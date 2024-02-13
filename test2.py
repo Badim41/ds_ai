3760d29a7de6fd6afe4e611b673394edc6699b59
badim41 <ramdomakk@gmail.com>
2024-02-13 22:30:38 +1200

    update

--- test2.py
+++ test2.py
@@ -1,11 +1,14 @@
 import os
 import sys
 from IPython.display import display, FileLink
+
 file = sys.argv[1]
 if os.path.exists(file):
     print("exists")
 else:
     print("not exists")
-FileLink(file)
-display(FileLink(file))
+
+file_link = FileLink(file)
+display(file_link)
+
 print("done")
