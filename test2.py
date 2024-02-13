d7496f852cc9d2cd989f297a4d254e720f3138cc
badim41 <ramdomakk@gmail.com>
2024-02-13 22:20:34 +1200

    update

--- test2.py
+++ test2.py
@@ -1,4 +1,2 @@
-sentences = [1, 2, 3, 4, 5]
-for i, sentence in enumerate(sentences):
-    if i == len(sentences) - 1:
-        print("last", i)
+from IPython.display import FileLink
+FileLink('ds_ai/voice_models.zip')
