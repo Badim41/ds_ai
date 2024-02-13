a42661a5ebf641df6464151bce9d188a0204174a
badim41 <ramdomakk@gmail.com>
2024-02-13 22:28:22 +1200

    update

--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
+import os
+import sys
 from IPython.display import display, FileLink
-display(FileLink('ds_ai/voice_models.zip'))
+file = sys.argv[1]
+if os.path.exists(file):
+    print("exists")
+else:
+    print("not exists")
+FileLink(file)
+display(FileLink(file))
 print("done")
