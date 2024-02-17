7e833a4cc9a7ad660d0f2280290ac735f8a03ceb
badim41 <ramdomakk@gmail.com>
2024-02-17 20:20:41 +1200

    update

--- discord_bot.py
+++ discord_bot.py
@@ -314,7 +314,7 @@ async def __generate_video(ctx,
                            ):
     async def repeat_generate_videos(seed, i):
         try:
-            seed = random.randint(1, 9999999999) if seed is None else seed // i
+            seed = random.randint(1, 9999999999) if seed is None else seed // i + 1
 
             cuda_number = await cuda_manager.use_cuda()
 
@@ -379,7 +379,7 @@ async def __generate_audio(ctx,
                            ):
     async def generate_audios_async(seed, i):
         try:
-            seed = random.randint(1, 9999999999) if seed is None else seed // i
+            seed = random.randint(1, 9999999999) if seed is None else seed // i + 1
 
             cuda_number = await cuda_manager.use_cuda()
             timer = Time_Count()
@@ -446,7 +446,7 @@ async def __generate_image(ctx,
                     style=style, x=x, y=y
                 )
             else:
-                seed = random.randint(1, 9999999999) if seed is None else seed // i
+                seed = random.randint(1, 9999999999) if seed is None else seed // i + 1
                 seed_text = f"\nСид:{seed}"
                 cuda_number = await cuda_manager.use_cuda()
 
@@ -513,7 +513,7 @@ async def __image_change(ctx,
     async def images_change_async(seed, i):
         try:
             cuda_number = await cuda_manager.use_cuda()
-            seed = random.randint(1, 9999999999) if seed is None else seed // i
+            seed = random.randint(1, 9999999999) if seed is None else seed // i + 1
 
             timer = Time_Count()
 
